a7ae98979826f4e945f1dd65c070e8fd6110612e
Alberto Medina <albertomedina@google.com>
2017-06-07 19:00:52 -0700

    First part of integrating AMP Start Article template

--- filters/content-filter.php
+++ filters/content-filter.php
@@ -1,10 +1,16 @@
 <?php
-
+//
+// TODO(@amedina): invoke from here the functionality of creating the
+// TODO(@amedina):AMP_Content class and generating the AMP content
 add_filter( 'the_content', 'filter_the_content_in_the_main_loop' );
 function filter_the_content_in_the_main_loop( $content ) {
 	// Check if we're inside the main loop in a single post page.
 	if ( is_single() && in_the_loop() && is_main_query() ) {
-		return $content . '<span style="color:red">' . "I'm filtering the content inside the main loop" . '</span>';
+		if ( get_theme_mod( 'markup' ) == "AMP" ) {
+			return $content . '<span style="color:red">' . "Content passed through the AMP filter!" . '</span>';
+		} else {
+			return $content . '<span style="color:blue">' . "Content was NOT passed through the AMP filter!" . '</span>';
+		}
 	}
 	return $content;
 }
--- functions.php
+++ functions.php
@@ -17,3 +17,47 @@
  *  Load content filters
  */
 require get_stylesheet_directory() . '/filters/content-filter.php';
+
+function ampruntime_js() {
+	echo '<script async src="https://cdn.ampproject.org/v0.js"></script>';
+}
+
+function amp_boilerplate_css() {
+	echo '<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>';
+}
+
+function amp_social_share_js() {
+	echo '<script custom-element="amp-social-share" src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js" async=""></script>';
+}
+
+function amp_custom_css() {
+	$styles = wp_get_custom_css();
+	$safe_styles = '';
+	if ( $styles || is_customize_preview() ) {
+		$safe_styles = strip_tags( $styles );
+	}
+
+	echo '<style amp-custom>' .file_get_contents(get_stylesheet_uri()) . $safe_styles . '</style>';
+}
+
+add_action( 'wp_head', 'ampruntime_js', 0 );
+add_action( 'wp_head', 'amp_boilerplate_css', 0 );
+add_action( 'wp_head', 'amp_custom_css', 0 );
+add_action( 'wp_head', 'amp_social_share_js', 0 );
+
+function wpcustom_inspect_scripts_and_styles() {
+	global $wp_scripts;
+	global $wp_styles;
+	$scripts_list = '';
+	$styles_list = '';
+	// Runs through the queue scripts
+	foreach( $wp_scripts->queue as $handle ) :
+		$scripts_list .= $handle . ' | ';
+	endforeach;
+
+	// Runs through the queue styles
+	foreach( $wp_styles->queue as $handle ) :
+		$styles_list .= $handle . ' | ';
+	endforeach;
+}
+//add_action( 'wp_print_scripts', 'wpcustom_inspect_scripts_and_styles' );
